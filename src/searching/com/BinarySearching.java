fc3aa8440e96e81222c810ae172cbcf7ec097545
Bishwas KC <bishwaskc777@gmail.com>
2025-03-10 16:33:52 +0545

    linearSearching

--- src/searching/com/BinarySearching.java
+++ src/searching/com/BinarySearching.java
@@ -2,25 +2,49 @@ package searching.com;
 
 public class BinarySearching {
 
-    public static int binarySearch(int array[],int target){
+    public static int binarySearch(int array[], int target) {
+
+        int steps = 0;
+
         //set the first value of array start with zero
-        int low = 0;
+        int left = 0;
 
         //get the max index
-        int high = array.length-1;
+        int right = array.length - 1;
 
         //In binary we need the mid value first+last/2
-        int midIndex=array[low]+array[high]/2;
-        int midValue=array[midIndex];
-
-
-        if(midValue==target){
-            return midValue;
-        }else if(midValue>target){
-            // we have to search in left side from midvalue
-            high = midIndex-1;
-            low = 0;
+        while (left <= right) {
+            steps++;
+            int mid = (left + right) / 2;
+            if (array[mid] == target) {
+                System.out.printf("Number of steps %s : %n", steps);
+                return mid;
+            } else if (array[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+            steps++;
+            System.out.println();
         }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[200];
+        for(int i =0;i<200;i++){
+            array[i]=i;
+        }
+        int target = 0;
+        int result = binarySearch(array, target);
+
+        if (result == -1) {
+            System.out.println("Element not found");
+        }
+        {
+            System.out.println("Element found in index : " + result);
+        }
+
     }
 
 }
--- src/searching/com/linearSearching.java
+++ src/searching/com/linearSearching.java
@@ -1,4 +1,31 @@
 package searching.com;
 
 public class linearSearching {
+
+    public static int linearSearching(int[] array, int target) {
+        int steps = 0;
+
+        for (int i = 0; i < array.length-1; i++) {
+            steps++;
+            if (array[i] == target) {
+                System.out.println("Number of steps : "+steps);
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int array[] = new int[200];
+        for(int i =0;i<=200;i++){
+            array[i]=i;
+        }
+        int target = 199;
+        int result = linearSearching(array, target);
+        if(result == -1){
+            System.out.println("Element not found!!");
+        }else{
+            System.out.println("Element found in index : "+result);
+        }
+    }
 }
